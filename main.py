00c2d822cadae4365e5d2bf80cecb9ba757f000e
Grzegorz <grzegorztokarz4634@gmail.com>
2023-04-15 09:37:06 +0200

    First version of program

--- main.py
+++ main.py
@@ -1,16 +1,78 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import requests
+import customtkinter as tk
+from tkcalendar import Calendar
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class RadioButton(tk.CTkRadioButton):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+class App(tk.CTk):
+
+
+    def __init__(self):
+        super().__init__()
+        self.selected_currency = tk.StringVar()
+
+        self.geometry("500x300")
+        self.title("Kursy walut")
+        self.minsize(1000, 1100)
+
+        self.label = tk.CTkLabel(master=self, text="1. Wybierz walutę", width=120, height=25, fg_color="#57c1fa",
+                                 corner_radius=8)
+        self.label.grid(row=0, column=3)
+        self.createRadioButtons()
+
+        self.label12 = tk.CTkLabel(master=self, text="2. Wybierz datę początkową", width=120, height=25,
+                                   fg_color="#57c1fa", corner_radius=8)
+        self.label12.grid(row=6, column=0 )
+        self.cal = Calendar(self, selectmode='day', year=2023, month=4, day=5)
+        self.cal.grid(row=7, column=0, columnspan=2)
+
+        self.label2 = tk.CTkLabel(master=self, text="3. Wybierz datę końcową", width=120, height=25,
+                                  fg_color=("#57c1fa"), corner_radius=8)
+        self.label2.grid(row=6, column=4)
+        self.cal1 = Calendar(self, selectmode='day', year=2023, month=4, day=5)
+        self.cal1.grid(row=7, column=4, padx=20, columnspan=2)
+
+        self.button = tk.CTkButton(master=self, text="Sprawdź kurs", command=self.getRates, width=120, height=100)
+        self.button.grid(row=8, column=3, padx=20, pady=20)
+
+
+    def createRadioButtons(self):
+        currency = ['EUR', 'USD', 'CHF', 'GBP', 'JPY']
+        for i in range(len(currency)):
+            radioButton = RadioButton(self, text=currency[i], variable=self.selected_currency, value=currency[i])
+            radioButton.grid(row=i+1, column=3, sticky='ns')
+
+    def changeString(self, string):
+        nstring = string.split('/')
+        nstring1 = '20'+nstring[2]+'-'+(nstring[0] if len(nstring[0])==2 else '0'+nstring[0])+'-'+(nstring[1] if len(nstring[1])==2 else '0'+nstring[1])
+        return nstring1
+    def plotGraph(self, rates, dates):
+        fig = plt.figure(figsize=(6, 4), dpi=100)
+        ax = fig.add_subplot(111)
+        ax.plot(dates, rates, linewidth=2)
+        ax.set_xlabel('Data')
+        ax.set_ylabel('Kurs')
+        canvas = FigureCanvasTkAgg(fig, master=self)
+        canvas.draw()
+        canvas.get_tk_widget().grid(row=9, column=0, columnspan=5, padx=25, pady=25)
+    def getRates(self):
+        response = requests.get(f"http://api.nbp.pl/api/exchangerates/rates/A/{self.selected_currency.get()}/{self.changeString(self.cal.get_date())}/{self.changeString(self.cal1.get_date())}/")
+        data = response.json()['rates']
+        dates=[]
+        rates=[]
+        for item in data:
+            dates.append(item['effectiveDate'])
+            rates.append(item['mid'])
+        self.plotGraph(rates, dates)
+        print("Wybrano opcję:", data)
+
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
